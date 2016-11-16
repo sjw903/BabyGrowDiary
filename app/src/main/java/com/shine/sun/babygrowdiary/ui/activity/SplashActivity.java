d06bd8254a1d130f0cd23e0f3a4b76967d377786
jtpay <18501358643@qq.com>
2016-11-16 09:12:06 +0800

    splash activity forbid back key

--- app/src/main/java/com/shine/sun/babygrowdiary/ui/activity/SplashActivity.java
+++ app/src/main/java/com/shine/sun/babygrowdiary/ui/activity/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 
 /**
  * Created by Steven on 2016/11/5 0005.
@@ -14,12 +15,13 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                SplashActivity.this.startActivity(new Intent(SplashActivity.this, HomeActivity.class));
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            SplashActivity.this.startActivity(new Intent(SplashActivity.this, HomeActivity.class));
         }, 2000L);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return false;
     }
 }
